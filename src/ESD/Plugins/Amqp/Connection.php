60c4df50126175c51c4ab996c08ae362a5eb85ad
bearlord <565364226@qq.com>
2020-09-25 23:27:29 +0800

    new file:   ESD/Plugins/Amqp/Connection.php 	new file:   ESD/Plugins/Amqp/HostConfig.php

---
+++ src/ESD/Plugins/Amqp/Connection.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * ESD framework
+ * @author tmtbe <896369042@qq.com>
+ */
+
+namespace ESD\Plugins\Amqp;
+
+use Exception;
+
+/**
+ * Class Connection
+ * @package ESD\Plugins\Amqp
+ */
+class Connection
+{
+
+    /**
+     * @var AmqpConfig
+     */
+    protected $config;
+
+    /**
+     * @var AMQPStreamConnection
+     */
+    protected $connection = null;
+
+    /**
+     * AmqpConnection constructor.
+     * @param Config $config
+     * @throws Exception
+     */
+    public function __construct(Config $config)
+    {
+        $config->buildConfig();
+        $this->config = $config;
+        $this->connect();
+    }
+
+    /**
+     * Get channel
+     * @param null $channel_id
+     * @return AmqpChannel
+     * @throws Exception
+     */
+    public function channel($channel_id = null)
+    {
+        return $this->connection->channel($channel_id);
+    }
+
+
+    /**
+     * Connect
+     * @throws Exception
+     */
+    protected function connect()
+    {
+        if($this->connection && !$this->connection->isConnected()) {
+            $this->connection->reconnect();
+        } else if (!$this->connection) {
+            foreach ($this->config->getHosts() as $key => $value) {
+                $connection = new \AMQPConnection($value);
+                try {
+                    $connection->connect();
+                    if ($connection->isConnected()) {
+                        $this->connection =  $connection;
+                    }
+                    return true;
+                } catch (\Exception $exception) {
+                    throw $exception;
+                }
+            }
+        }
+    }
+
+    /**
+     * @return Config
+     */
+    public function getConfig(): Config
+    {
+        return $this->config;
+    }
+
+    /**
+     * @param Config $config
+     */
+    public function setConfig(Config $config): void
+    {
+        $this->config = $config;
+    }
+}
