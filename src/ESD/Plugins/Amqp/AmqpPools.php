d0d3e0261073a8201ba8415d864f8caae385444b
bearlord <565364226@qq.com>
2020-09-29 20:53:03 +0800

    new file:   Plugins/Amqp/AmqpPools.php

---
+++ src/ESD/Plugins/Amqp/AmqpPools.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * ESD framework
+ * @author tmtbe <896369042@qq.com>
+ */
+
+namespace ESD\Plugins\Amqp;
+
+/**
+ * Class AmqpPools
+ * @package ESD\Yii\Plugin\Amqp
+ */
+class AmqpPools
+{
+    /**
+     * @var array
+     */
+    protected $poolList = [];
+
+    /**
+     * Get pool
+     *
+     * @param string $name
+     * @return AmqpPool
+     */
+    public function getPool($name = "default")
+    {
+        return $this->poolList[$name] ?? null;
+    }
+
+    /**
+     * @param AmqpPool $pool
+     */
+    public function addPool(AmqpPool $pool)
+    {
+        $this->poolList[$pool->getConfig()->getName()] = $pool;
+    }
+}
