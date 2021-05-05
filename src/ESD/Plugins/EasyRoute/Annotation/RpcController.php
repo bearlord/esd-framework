e4195e41826b57edf98d1ed6ca62bf68cde67fe5
bearlod <565364226@qq.com>
2021-05-05 12:18:33 +0800

    new file:   src/ESD/Plugins/EasyRoute/Annotation/RpcController.php 	new file:   src/ESD/Plugins/RpcClient/.gitkeep 	new file:   src/ESD/Plugins/RpcServer/.gitkeep

---
+++ src/ESD/Plugins/EasyRoute/Annotation/RpcController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * ESD framework
+ * @author bearload <565364226@qq.com>
+ */
+
+namespace ESD\Plugins\EasyRoute\Annotation;
+
+/**
+ * @Annotation
+ * @Target("CLASS")
+ * Class RpcController
+ * @package ESD\Plugins\EasyRoute\Annotation
+ */
+class RpcController extends Controller
+{
+    
+}
