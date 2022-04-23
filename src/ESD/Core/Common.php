26fd6fa142a8b4dea6754b5ae5965746241123bd
bearlord <565364226@qq.com>
2022-04-24 00:11:47 +0800

    added call function

--- src/ESD/Core/Common.php
+++ src/ESD/Core/Common.php
@@ -249,3 +249,26 @@ function DISet($name, $value)
 {
     DI::getInstance()->set($name, $value);
 }
+
+
+/**
+ * Call a callback with the arguments.
+ *
+ * @param mixed $callback
+ * @return null|mixed
+ */
+function call($callback, array $args = [])
+{
+    $result = null;
+    if ($callback instanceof \Closure) {
+        $result = $callback(...$args);
+    } elseif (is_object($callback) || (is_string($callback) && function_exists($callback))) {
+        $result = $callback(...$args);
+    } elseif (is_array($callback)) {
+        [$object, $method] = $callback;
+        $result = is_object($object) ? $object->{$method}(...$args) : $object::$method(...$args);
+    } else {
+        $result = call_user_func_array($callback, $args);
+    }
+    return $result;
+}
