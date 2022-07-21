36a6d7805662e89f8447952277720ece28071b13
bearlord <565364226@qq.com>
2022-07-21 18:48:01 +0800

    debug

---
+++ src/ESD/Plugins/Amqp/Connection/AMQPSwooleConnection.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ESD\Plugins\Amqp\Connection;
+
+use PhpAmqpLib\Connection\AbstractConnection;
+
+class AMQPSwooleConnection extends AbstractConnection
+{
+    public function __construct(
+        string $host,
+        int $port,
+        string $user,
+        string $password,
+        string $vhost = '/',
+        bool $insist = false,
+        string $loginMethod = 'AMQPLAIN',
+        $loginResponse = null,
+        string $locale = 'en_US',
+        float $connectionTimeout = 3.0,
+        float $readWriteTimeout = 3.0,
+        $context = null,
+        bool $keepalive = false,
+        int $heartbeat = 0
+    ) {
+        if ($keepalive) {
+            $io = new KeepaliveIO($host, $port, $connectionTimeout, $readWriteTimeout, $context, $keepalive, $heartbeat);
+        } else {
+            $io = new SwooleIO($host, $port, $connectionTimeout, $readWriteTimeout, $context, $keepalive, $heartbeat);
+        }
+
+        parent::__construct(
+            $user,
+            $password,
+            $vhost,
+            $insist,
+            $loginMethod,
+            $loginResponse,
+            $locale,
+            $io,
+            $heartbeat,
+            (int) $connectionTimeout
+        );
+    }
+
+    public function getIO()
+    {
+        return $this->io;
+    }
+}
---
+++ src/ESD/Plugins/Amqp/Connection/Socket.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace ESD\Plugins\Amqp\Connection;
+
+use Hyperf\Contract\StdoutLoggerInterface;
+use Hyperf\Utils\ApplicationContext;
+use PhpAmqpLib\Exception\AMQPRuntimeException;
+use PhpAmqpLib\Wire\AMQPWriter;
+use Psr\Log\LoggerInterface;
+use Swoole\Coroutine\Channel;
+use Swoole\Coroutine\Client;
+use Swoole\Timer;
+
+class Socket
+{
+    /**
+     * @var Channel
+     */
+    protected $channel;
+
+    /**
+     * @var null|int
+     */
+    protected $timerId;
+
+    /**
+     * @var bool
+     */
+    protected $connected = false;
+
+    /**
+     * @var string
+     */
+    protected $host;
+
+    /**
+     * @var int
+     */
+    protected $port;
+
+    /**
+     * @var float
+     */
+    protected $timeout;
+
+    /**
+     * @var int
+     */
+    protected $heartbeat;
+
+    /**
+     * @var float
+     */
+    protected $waitTimeout = 10.0;
+
+    /**
+     * @param string $host
+     * @param int $port
+     * @param float $timeout
+     * @param int $heartbeat
+     */
+    public function __construct(string $host, int $port, float $timeout, int $heartbeat)
+    {
+        $this->host = $host;
+        $this->port = $port;
+        $this->timeout = $timeout;
+        $this->heartbeat = $heartbeat;
+
+        $this->connect();
+    }
+
+
+    /**
+     * @param \Closure $closure
+     * @return mixed
+     */
+    public function call(\Closure $closure)
+    {
+        if (!$this->isConnected()) {
+            $this->connect();
+        }
+
+        $client = $this->channel->pop($this->waitTimeout);
+
+        if ($client === false) {
+            throw new AMQPRuntimeException('Socket of keepaliveIO is exhausted. Cannot establish new socket before wait_timeout.');
+        }
+
+        try {
+            $result = $closure($client);
+        } finally {
+            $this->channel->push($client);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return void
+     */
+    public function connect()
+    {
+        $sock = new Client(SWOOLE_SOCK_TCP);
+        if (!$sock->connect($this->host, $this->port, $this->timeout)) {
+            throw new AMQPRuntimeException(
+                sprintf(
+                    'Error Connecting to server(%s): %s ',
+                    $sock->errCode,
+                    swoole_strerror($sock->errCode)
+                ),
+                $sock->errCode
+            );
+        }
+
+        $this->channel = new Channel(1);
+        $this->channel->push($sock);
+        $this->connected = true;
+
+        $this->addHeartbeat();
+    }
+
+    /**
+     * @return bool
+     */
+    public function isConnected(): bool
+    {
+        return $this->connected;
+    }
+
+    public function close()
+    {
+        $this->connected = false;
+        $this->clear();
+    }
+
+    /**
+     * Sends a heartbeat message.
+     */
+    public function heartbeat()
+    {
+        $pkt = new AMQPWriter();
+        $pkt->write_octet(8);
+        $pkt->write_short(0);
+        $pkt->write_long(0);
+        $pkt->write_octet(0xCE);
+        $data = $pkt->getvalue();
+
+        $this->call(function ($client) use ($data) {
+            $buffer = $client->send($data);
+            if ($buffer === false) {
+                throw new AMQPRuntimeException('Error sending data');
+            }
+        });
+    }
+
+    /**
+     * @return void
+     */
+    protected function addHeartbeat()
+    {
+        $this->clear();
+        $this->timerId = Timer::tick($this->heartbeat * 1000, function () {
+            try {
+                if ($this->isConnected()) {
+                    $this->heartbeat();
+                }
+            } catch (\Throwable $throwable) {
+//                $this->close();
+
+                $message = sprintf('KeepaliveIO heartbeat failed, %s', (string)$throwable);
+                DIGet(LoggerInterface::class)->error($message);
+            }
+        });
+    }
+
+    protected function clear()
+    {
+        if ($this->timerId) {
+            Timer::clear($this->timerId);
+            $this->timerId = null;
+        }
+    }
+
+    public function __destruct()
+    {
+        $this->clear();
+    }
+}
