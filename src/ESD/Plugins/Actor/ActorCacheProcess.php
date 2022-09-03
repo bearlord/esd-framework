1164fe7c21bc61dff686ba4a6a352019db4bd992
bealord <565364226@qq.com>
2022-09-03 21:40:38 +0800

    dev:Actor

--- src/ESD/Plugins/Actor/ActorCacheProcess.php
+++ src/ESD/Plugins/Actor/ActorCacheProcess.php
@@ -79,6 +79,7 @@ class ActorCacheProcess extends Process
     {
         $this->cacheHash = new ActorCacheHash($this);
 
+        Server::$instance->getLog()->error(Server::$instance->getConfigContext()->get('actor.autoSaveTime'));
         $this->setAutoSaveTime(Server::$instance->getConfigContext()->get('actor.autoSaveTime'));
     }
 
