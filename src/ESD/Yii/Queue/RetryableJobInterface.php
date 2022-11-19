3207752599f80671cb5d1733718aeacdbe884784
bearlord <565364226@qq.com>
2022-11-19 10:59:32 +0800

    add: RetryableJobInterface

---
+++ src/ESD/Yii/Queue/RetryableJobInterface.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace ESD\Yii\Queue;
+
+/**
+ * Retryable Job Interface.
+ *
+ * @author Roman Zhuravlev <zhuravljov@gmail.com>
+ */
+interface RetryableJobInterface extends JobInterface
+{
+    /**
+     * @return int time to reserve in seconds
+     */
+    public function getTtr();
+
+    /**
+     * @param int $attempt number
+     * @param \Exception|\Throwable $error from last execute of the job
+     * @return bool
+     */
+    public function canRetry($attempt, $error);
+}
