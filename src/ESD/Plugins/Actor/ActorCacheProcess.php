9b04b10636364e74fc8dedee1e24c4ee1f2d2c55
bearlord <565364226@qq.com>
2023-08-11 16:24:57 +0800

    [upgrade]Actor-cache

--- src/ESD/Plugins/Actor/ActorCacheProcess.php
+++ src/ESD/Plugins/Actor/ActorCacheProcess.php
@@ -24,6 +24,8 @@ class ActorCacheProcess extends Process
 
     const HASH_KEY = "actor-cache";
 
+    const KEY_PREFIX = "actor-single-";
+
     /**
      * @var float|int auto save time
      */
@@ -202,12 +204,23 @@ class ActorCacheProcess extends Process
         goWithContext(function () {
             $saveData = [];
             if (!empty($this->cacheHash->getContainer())) {
+                /*
                 foreach ($this->cacheHash[self::SAVE_NAME] as $key => $value) {
                     $saveData[$key] = json_encode($value, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
                 }
 
                 $this->redis()->del(self::HASH_KEY);
                 $this->redis()->hMSet(self::HASH_KEY, $saveData);
+                */
+                foreach ($this->cacheHash[self::SAVE_NAME] as $key => $value) {
+                    $valueJson = json_encode($value, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+
+                    $redisKey = self::KEY_PREFIX . $key;
+                    $this->redis()->del($redisKey);
+                    $this->redis()->set($redisKey, $valueJson);
+
+                    Coroutine::sleep(0.001);
+                }
             }
         });
     }
@@ -219,11 +232,24 @@ class ActorCacheProcess extends Process
      */
     protected function recovery()
     {
+        /*
         $acotrs = $this->redis()->hGetAll(self::HASH_KEY);
         if (!empty($acotrs)) {
             foreach ($acotrs as $key => $value) {
                 $valueArray = json_decode($value, true);
                 Actor::create($valueArray[0], $valueArray[1], $valueArray[2], false, 30);
+                Coroutine::sleep(0.001);
+            }
+        }
+        */
+
+        $actorKeys = $this->redis()->keys(self::KEY_PREFIX . "*");
+        if (!empty($actorKeys)) {
+            foreach ($actorKeys as $k => $v) {
+                $value = $this->redis()->get($v);
+                $valueArray = json_decode($value, true);
+                Actor::create($valueArray[0], $valueArray[1], $valueArray[2], false, 30);
+
                 Coroutine::sleep(0.001);
             }
         }
