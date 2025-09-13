918cc936638a798ef5bbbf5e5ea658be610025b3
bearlord <565364226@hotmail.com>
2025-09-13 15:49:22 +0800

    [fix]RateLimitAspect

--- src/ESD/Plugins/RateLimit/Aspect/RateLimitAspect.php
+++ src/ESD/Plugins/RateLimit/Aspect/RateLimitAspect.php
@@ -125,6 +125,9 @@ class RateLimitAspect extends OrderAspect
                             $clientData->getResponse()->withContent(Json::encode($callResult))->end();
 
                             break;
+
+                        default:
+                            return $invocation->proceed();
                     }
                 }
                 break;
